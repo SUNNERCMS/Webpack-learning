aafefdd820e8a5dd835140a26b29a6837bc8fbbf
zhaoxiang.sun <zhaoxiang.sun@qunar.com>
2019-07-27 00:48:38 +0800

    fivetox5

--- gittest/hello.py
+++ gittest/hello.py
@@ -1,11 +1,7 @@
 print 'line2'
 print 'line3'
 print 'line4'
-<<<<<<< HEAD
-print 'linefive'
-=======
 print 'xxxx5'
->>>>>>> eb281bde16c921e31c68ea6243a47632e482fa12
 print 'line6'
 print 'line7'
 print 'line8'
